6f35bbe1281a9e90bb32b0b7eff6ee2a8eef16e7
Bryan O'Sullivan <bos@serpentine.com>
2010-01-07 23:34:36 -0800

    Switch to a configure-based build.

--- Setup.hs
+++ Setup.hs
@@ -1,12 +1,6 @@
 module Main (main) where
 
-import Distribution.Simple (defaultMainWithHooks, runTests, simpleUserHooks)
-import System.Cmd (system)
+import Distribution.Simple
 
 main :: IO ()
-main =
-    defaultMainWithHooks $ simpleUserHooks { runTests = runTests' }
-  where
-    runTests' _ _ _ _ = do
-        system "./dist/build/test/test -j4"
-        return ()
+main = defaultMainWithHooks defaultUserHooks
