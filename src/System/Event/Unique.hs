5bfcd164584e9a1c217368942fc4192a11364d6b
Bryan O'Sullivan <bos@serpentine.com>
2010-01-10 21:55:53 -0800

    Missed a file!

---
+++ src/System/Event/Unique.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE BangPatterns #-}
+module System.Event.Unique
+    (
+      UniqueSource
+    , Unique(..)
+    , newSource
+    , newUnique
+    ) where
+
+import Data.IORef (IORef, atomicModifyIORef, newIORef)
+import Data.Int (Int64)
+
+newtype UniqueSource = US (IORef Int64)
+
+newtype Unique = Unique { asInt64 :: Int64 }
+    deriving (Eq, Ord, Show)
+
+newSource :: IO UniqueSource
+newSource = US `fmap` newIORef 0
+
+newUnique :: UniqueSource -> IO Unique
+newUnique (US ref) = atomicModifyIORef ref $ \u ->
+                     let !u' = u+1 in (u', Unique u)
