aaaeb7a17577f603ca54decae33d10bc1c6f5f27
Bryan O'Sullivan <bos@serpentine.com>
2010-01-19 23:06:28 -0800

    Fix a nasty race condition spotted by Mark Lentczner.
    In the interval between updating the map and telling the back end which
    events to listen for, it was possible for another thread to sneak in and
    give the back end a possibly clashing event mask, leading to inconsistency
    between what the manager and back end thought they were waiting for.
    
    We now wrap the emFds element in an MVar that we hold for the entire
    duration of modifying both the manager and the back end.  This costs us a
    little performance, but the inter-measurement variation is large enough that
    it swamps any difference, so I conclude that it must not be significant.

--- src/System/Event/Manager.hs
+++ src/System/Event/Manager.hs
@@ -19,7 +19,6 @@ module System.Event.Manager
     , FdRegistration
     , registerFd_
     , registerFd
-    , unregisterFd__
     , unregisterFd_
     , unregisterFd
     , fdWasClosed
@@ -37,18 +36,19 @@ module System.Event.Manager
 ------------------------------------------------------------------------
 -- Imports
 
+import Control.Concurrent.MVar (MVar, modifyMVar, modifyMVar_, newMVar, readMVar)
 import Control.Monad (forM_, when)
-import Data.IORef
+import Data.IORef (IORef, atomicModifyIORef, newIORef, readIORef, writeIORef)
 import Data.Monoid (mconcat, mempty)
 import System.Posix.Types (Fd)
 
-import System.Event.Clock
+import System.Event.Clock (getCurrentTime)
 import System.Event.Internal (Backend, Event, evtRead, evtWrite, Timeout(..))
 import qualified System.Event.Internal as I
 import qualified System.Event.PSQ as Q
 import System.Event.Control
 import qualified System.Event.IntMap as IM
-import System.Event.Unique
+import System.Event.Unique (Unique, UniqueSource, newSource, newUnique)
 
 #if defined(HAVE_KQUEUE)
 import qualified System.Event.KQueue as KQueue
@@ -83,7 +83,7 @@ type TimeoutCallback = IO ()
 -- | The event manager state.
 data EventManager = forall a. Backend a => EventManager
     { emBackend      :: !a
-    , emFds          :: !(IORef (IM.IntMap [FdData]))
+    , emFds          :: !(MVar (IM.IntMap [FdData]))
     , emTimeouts     :: !(IORef (Q.PSQ TimeoutCallback))
     , emKeepRunning  :: !(IORef Bool)
     , emUniqueSource :: !UniqueSource
@@ -120,13 +120,13 @@ new = newWith =<< newDefaultBackend
 
 newWith :: I.Backend b => b -> IO EventManager
 newWith be = do
-  iocbs <- newIORef IM.empty
+  iofds <- newMVar IM.empty
   timeouts <- newIORef Q.empty
   ctrl <- newControl
   run <- newIORef True
   us <- newSource
   let mgr = EventManager { emBackend = be
-                         , emFds = iocbs
+                         , emFds = iofds
                          , emTimeouts = timeouts
                          , emKeepRunning = run
                          , emUniqueSource = us
@@ -176,15 +176,15 @@ registerFd_ :: EventManager -> IOCallback -> Fd -> Event
             -> IO (FdRegistration, Bool)
 registerFd_ EventManager{..} cb fd evs = do
   u <- newUnique emUniqueSource
-  let fd' = fromIntegral fd
-  (oldEvs, newEvs) <- atomicModifyIORef emFds $ \f ->
-    case IM.insertLookupWithKey (const (++)) fd' [FdData u evs cb] f of
-      (Nothing,   newMap) -> (newMap, (mempty, evs))
-      (Just prev, newMap) -> (newMap, pairEvents prev newMap fd')
-  let modify = oldEvs /= newEvs
-  when modify $ I.modifyFd emBackend fd oldEvs newEvs
-  let !reg = FdRegistration fd u
-  return (reg, modify)
+  modifyMVar emFds $ \oldMap -> do
+    let fd' = fromIntegral fd
+        (!newMap, (oldEvs, newEvs)) = case IM.insertLookupWithKey (const (++)) fd' [FdData u evs cb] oldMap of
+                             (Nothing,   n) -> (n, (mempty, evs))
+                             (Just prev, n) -> (n, pairEvents prev newMap fd')
+        modify = oldEvs /= newEvs
+    when modify $ I.modifyFd emBackend fd oldEvs newEvs
+    let !reg = FdRegistration fd u
+    return (newMap, (reg, modify))
 {-# INLINE registerFd_ #-}
 
 -- | @registerFd mgr cb fd evs@ registers interest in the events @evs@
@@ -211,30 +211,22 @@ pairEvents prev m fd = let !l = eventsOf prev
                                   Just fds -> eventsOf fds
                        in (l, r)
 
--- | Drop a previous file descriptor registration, without modifying
--- the event manager's back end.  The return value contains the old
--- and new values of the events watched for this file descriptor.
-unregisterFd__ :: EventManager -> FdRegistration -> IO (Event, Event)
-unregisterFd__ mgr (FdRegistration fd u) = do
-  let dropReg _ cbs = case filter ((/= u) . fdUnique) cbs of
-                        []   -> Nothing
-                        cbs' -> Just cbs'
-      fd' = fromIntegral fd
-  v@(!_, !_) <- atomicModifyIORef (emFds mgr) $ \f ->
-    case IM.updateLookupWithKey dropReg fd' f of
-      (Nothing,   _)      -> (f,      (mempty, mempty))
-      (Just prev, newMap) -> (newMap, pairEvents prev newMap fd')
-  return v
-
 -- | Drop a previous file descriptor registration, without waking the
 -- event manager thread.  The return value indicates whether the event
 -- manager needs to be woken.
 unregisterFd_ :: EventManager -> FdRegistration -> IO Bool
-unregisterFd_ mgr@EventManager{..} reg@(FdRegistration fd _) = do
-  (oldEvs, newEvs) <- unregisterFd__ mgr reg
-  let modify = oldEvs /= newEvs
-  when modify $ I.modifyFd emBackend fd oldEvs newEvs
-  return $! modify
+unregisterFd_ EventManager{..} (FdRegistration fd u) =
+  modifyMVar emFds $ \oldMap -> do
+    let dropReg _ cbs = case filter ((/= u) . fdUnique) cbs of
+                          []   -> Nothing
+                          cbs' -> Just cbs'
+        fd' = fromIntegral fd
+        (!newMap, (oldEvs, newEvs)) = case IM.updateLookupWithKey dropReg fd' oldMap of
+                                       (Nothing,   _)   -> (oldMap, (mempty, mempty))
+                                       (Just prev, new) -> (newMap, pairEvents prev newMap fd')
+        modify = oldEvs /= newEvs
+    when modify $ I.modifyFd emBackend fd oldEvs newEvs
+    return (newMap, modify)
 
 -- | Drop a previous file descriptor registration.
 unregisterFd :: EventManager -> FdRegistration -> IO ()
@@ -244,12 +236,13 @@ unregisterFd mgr reg = do
 
 -- | Notify the event manager that a file descriptor has been closed.
 fdWasClosed :: EventManager -> Fd -> IO ()
-fdWasClosed mgr fd = do
-  oldEvs <- atomicModifyIORef (emFds mgr) $ \f ->
-    case IM.updateLookupWithKey (\ _ _ -> Nothing) (fromIntegral fd) f of
-      (Nothing,  _)       -> (f,      mempty)
-      (Just fds, !newMap) -> (newMap, eventsOf fds)
-  when (oldEvs /= mempty) $ wakeManager mgr
+fdWasClosed mgr fd =
+  modifyMVar_ (emFds mgr) $ \oldMap ->
+    case IM.updateLookupWithKey (\ _ _ -> Nothing) (fromIntegral fd) oldMap of
+      (Nothing,  _)       -> return oldMap
+      (Just fds, !newMap) -> do
+        when (eventsOf fds /= mempty) $ wakeManager mgr
+        return newMap
 
 ------------------------------------------------------------------------
 -- Registering interest in timeout events
@@ -287,7 +280,7 @@ updateTimeout mgr key ms = do
 -- | Call the callbacks corresponding to the given file descriptor.
 onFdEvent :: EventManager -> Fd -> Event -> IO ()
 onFdEvent mgr fd evs = do
-    fds <- readIORef (emFds mgr)
+    fds <- readMVar (emFds mgr)
     case IM.lookup (fromIntegral fd) fds of
         Just cbs -> forM_ cbs $ \(FdData _ ev cb) ->
                       when (evs `I.eventIs` ev) $ cb fd evs
