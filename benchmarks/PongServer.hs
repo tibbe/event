377c836d764969cd70f58275ff581da5f1c574af
Johan Tibell <johan.tibell@gmail.com>
2010-01-28 08:28:53 -0800

    Make pong-server more flexible

--- benchmarks/PongServer.hs
+++ benchmarks/PongServer.hs
@@ -5,8 +5,12 @@
 -- Start server and run
 --   httperf --server=localhost --port=5002 --uri=/ --num-conns=10000
 
+import Args (ljust, parseArgs, positive, theLast)
+
 import Control.Concurrent (forkIO)
 import Data.ByteString.Char8 ()
+import Data.Function (on)
+import Data.Monoid (Monoid(..), Last(..))
 import Network (PortID(..), listenOn)
 import Network.Socket (Socket, sClose)
 #if 1
@@ -15,11 +19,16 @@ import EventSocket (accept, recv, sendAll)
 import Network.Socket (accept)
 import Network.Socket.ByteString (recv, sendAll)
 #endif
+import System.Console.GetOpt (ArgDescr(ReqArg), OptDescr(..))
+import System.Environment (getArgs)
 import System.Event.Thread (ensureIOManagerIsRunning)
 
 main = do
+    (cfg, _) <- parseArgs defaultConfig defaultOptions =<< getArgs
+    let port = fromIntegral $ theLast cfgPort cfg
+
     ensureIOManagerIsRunning
-    sock <- listenOn $ PortNumber 5002
+    sock <- listenOn $ PortNumber port
     acceptConnections sock
 
 acceptConnections :: Socket -> IO ()
@@ -37,3 +46,35 @@ client sock = do
     sClose sock
 
 msg = "HTTP/1.0 200 OK\r\n\r\nContent-Length: 5\r\n\r\nPong!\r\n"
+
+------------------------------------------------------------------------
+-- Configuration
+
+data Config = Config {
+      cfgPort :: Last Int
+    }
+
+defaultConfig :: Config
+defaultConfig = Config
+    { cfgPort = ljust 5002
+    }
+
+instance Monoid Config where
+    mempty = Config
+        { cfgPort     = mempty
+        }
+
+    mappend a b = Config
+        { cfgPort     = app cfgPort a b
+        }
+      where
+        app :: (Monoid b) => (a -> b) -> a -> a -> b
+        app = on mappend
+
+defaultOptions :: [OptDescr (IO Config)]
+defaultOptions = [
+      Option ['p'] ["port"]
+          (ReqArg (positive "server port" $ \n ->
+               mempty { cfgPort = n }) "N")
+          "server port"
+    ]
