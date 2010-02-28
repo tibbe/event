91a246b1456e707bca16f2839c513538de02acdd
Johan Tibell <johan.tibell@google.com>
2010-02-28 14:44:58 +0100

    Add profiling flags to the Makefiles

--- tests/common.mk
+++ tests/common.mk
@@ -1,3 +1,19 @@
+# To enable profiling you must pass the --enable-library-profiling
+# flag to "cabal configure".
+ghc-prof-flags :=
+ifdef ENABLE_PROFILING
+	ghc-prof-flags += -prof -hisuf p_hi -osuf p_o -auto-all
+	lib-suffix := _p
+else
+	lib-suffix :=
+endif
+
+# To enable event logging you must pass the --ghc-option=-eventlog
+# flag to "cabal configure".
+ifdef ENABLE_EVENTLOG
+	ghc-prof-flags += -eventlog
+endif
+
 package := event
 version := $(shell awk '/^version:/{print $$2}' ../$(package).cabal)
 ghc := ghc
@@ -9,7 +25,7 @@ ghc-base-flags += -Wall -fno-warn-orphans -fno-warn-missing-signatures
 ghc-flags := $(ghc-base-flags) -i../dist/build -package-name $(package)-$(version)
 ghc-hpc-flags := $(ghc-base-flags) -fhpc -fno-ignore-asserts -odir hpcdir \
 	-hidir hpcdir -i..
-lib := ../dist/build/libHS$(package)-$(version).a
+lib := ../dist/build/libHS$(package)-$(version)$(lib-suffix).a
 lib-srcs := $(shell grep '^    *System' ../$(package).cabal | \
                     sed -e 's,\.,/,g' -e 's,$$,.hs,')
 
