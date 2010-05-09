0b30d3415a774bfaf546321b2ba5e93655036e96
Bryan O'Sullivan <bos@lindenlab.com>
2010-05-09 15:07:14 -0700

    Add a test for issue #1
    See http://github.com/tibbe/event/issues/#issue/1 for the report

--- tests/Regression.hs
+++ tests/Regression.hs
@@ -1,7 +1,7 @@
 module Regression (tests) where
 
 import Control.Exception (bracket)
-import Test.HUnit (Assertion, assertBool, assertEqual)
+import Test.HUnit (Assertion)
 import qualified System.Event as E
 import qualified Test.Framework as F
 import qualified Test.Framework.Providers.HUnit as F
@@ -9,7 +9,15 @@ import qualified Test.Framework.Providers.HUnit as F
 startupShutdown :: Assertion
 startupShutdown = E.new >>= E.shutdown
 
+doubleRegister :: Assertion
+doubleRegister = bracket E.new E.shutdown $ \mgr -> do
+  let cb _ _ = return ()
+  _ <- E.registerFd_ mgr cb 1 E.evtRead
+  _ <- E.registerFd_ mgr cb 1 E.evtRead
+  return ()
+
 tests :: F.Test
 tests = F.testGroup "Regression" [
-         F.testCase "startupShutdown" startupShutdown
+          F.testCase "doubleRegister" doubleRegister
+        , F.testCase "startupShutdown" startupShutdown
         ]
