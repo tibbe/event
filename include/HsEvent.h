532cc1a8d1f54298307f4d50d9c4becdc62621a5
Johan Tibell <johan.tibell@google.com>
2010-06-03 17:27:30 +0200

    Reverted the previous select back-end commit

--- include/HsEvent.h
+++ include/HsEvent.h
@@ -6,19 +6,6 @@
 #include <signal.h>
 #include <pthread.h>
 
-#if defined(HAVE_SYS_SELECT_H)
-#include <sys/select.h>
-#endif
-#if defined(HAVE_SYS_TIME_H)
-#include <sys/time.h>
-#endif
-#if defined(HAVE_SYS_TYPES_H)
-#include <sys/types.h>
-#endif
-#if defined(HAVE_UNISTD_H)
-#include <unistd.h>
-#endif
-
 #if !defined(INLINE)
 # if defined(_MSC_VER)
 #  define INLINE extern __inline
@@ -44,21 +31,6 @@ INLINE void __hsevent_thread_self(pthread_t *tid)
 INLINE int __hsevent_kill_thread(pthread_t *tid, int sig)
 {
     return pthread_kill(*tid, sig);
-}
-
-INLINE int  __hsevent_fd_isset(int fd, fd_set *fds)
-{
-    return FD_ISSET(fd, fds);
-}
-
-INLINE void __hsevent_fd_set(int fd, fd_set *fds)
-{
-    FD_SET(fd, fds);
-}
-
-INLINE void __hsevent_fd_zero(fd_set *fds)
-{
-    FD_ZERO(fds);
 }
 
 #endif /* __HS_EVENT_H__ */
